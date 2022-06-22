fdb307d0a1f11557bb72268e61cd462237453c92
hukangkang <hukangkang@xiaomi.com>
2022-06-22 20:34:35 +0800

    first commit

---
+++ leetcode/two-sum/TwoSum.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print(twoSum([]int{2, 7, 11, 15}, 9))
+}
+
+func twoSum(nums []int, target int) []int {
+	// len(nums) 获取nums的长度
+	// 映射两种初始换的方式
+	// 1) make(map[int]int, len(nums))
+	// 2) map[string]string{"Red": "#da1337", "Orange": "#e95a22"}
+	var result = make(map[int]int, len(nums))
+	for index, num := range nums {
+		// value是map中的值，exist表示对应的key是否存在值
+		value, exist := result[target-num]
+		//也能像下面这样再一个语句中执行
+		//if j, ok := result[target-num]; ok {
+		if exist {
+			return []int{value, index}
+		}
+		result[num] = index
+	}
+	return nil
+}
