d71b33591968dad540a9988437455d6f5874b7d8
hukangkang <hukangkang@xiaomi.com>
2022-12-19 18:53:18 +0800

    123

---
+++ base/readfile/readfile.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+}
